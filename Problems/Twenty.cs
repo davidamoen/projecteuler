3126829616ce6efcd09a2a683536ff599c614b31
davidamoen <david.moen@solutiondesign.com>
2015-11-13 07:56:14 -0600

    twenty

--- Problems/Twenty.cs
+++ Problems/Twenty.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,8 @@ namespace ProjectEuler.Problems
         public static long Go()
         {
 
-
+            BigInteger n = Helpers.GetFactorial(100);
+            return Helpers.GetSumOfDigits(n);
         }
     }
 }
